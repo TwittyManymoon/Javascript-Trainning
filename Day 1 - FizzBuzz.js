f77fb41bf428c63bcce3108ec666e61b146c65f7
Twitty Manymoon <34795297+TwittyManymoon@users.noreply.github.com>
2018-12-27 16:31:55 +0700

    Create Day 1 - FizzBuzz.js

---
+++ Day 1 - FizzBuzz.js
@@ -0,0 +1,34 @@
+/* {
+// Day 1 :
+// 
+// Write a program that uses console.log to print all the numbers from 1 to 100, 
+// with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, 
+// and for numbers divisible by 5 (and not 3), print "Buzz" instead. 
+//
+// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 
+// (and still print "Fizz" or "Buzz" for numbers divisible by only one of those). 
+//
+// ::: developed by Twitty Manymoon :::
+}*/
+
+var i = 1;
+
+while (i <= 100) {
+    // FizzBuzz
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`${i} : FizzBuzz`);
+    }
+
+    // Fizz or Buzz
+    if (i % 3 == 0) {
+        console.log(`${i} : Fizz`);
+    }
+    else if (i % 5 == 0) {
+        console.log(`${i} : Buzz`);
+    }
+    // Neither Fizz nor Buzz
+    else {
+        console.log(`${i} : neither Fizz nor Buzz`);
+    }
+    i++;
+}
